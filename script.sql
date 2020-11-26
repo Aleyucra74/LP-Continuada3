25208648d4b71c579b92ecf255d916ffa6a4edab
Aleyucra74 <alexandre.yucra@outlook.com>
2020-11-26 20:56:16 -0300

    database connection done

---
+++ script.sql
@@ -0,0 +1,9 @@
+use continuada3;
+
+create table alunos(
+	id int auto_increment primary key,
+	nome varchar(50),
+	escola varchar(50),
+	materia varchar(50),
+	professor varchar(50)
+)
--- src/main/java/com/mycompany/lp/continuada3/ConexaoBanco.java
+++ src/main/java/com/mycompany/lp/continuada3/ConexaoBanco.java
@@ -6,9 +6,38 @@ import java.sql.SQLException;
 
 public class ConexaoBanco {
 
-    private final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    private final String URL = "";
-    private final String USERNAME = "adminlocal";
-    private final String PASSWORD = "#Gfgrupo11c";
-   
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost/Continuada3?useTimezone=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "123";
+  
+    public static void main(String[] args) {
+        ConexaoBanco conexao = new ConexaoBanco();
+        Connection conn = conexao.getConnection();
+        conexao.desconectar(conn);
+    }
+    
+    public static Connection getConnection(){
+        Connection conn = null;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("conectado ao mysql");
+        } catch (ClassNotFoundException | SQLException ex) {
+           throw new RuntimeException("Erro na conexão:", ex);
+        }
+        return conn;
+    }
+    
+    public void desconectar(Connection conn){
+        try {
+            if(conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("Desconectou do banco de dados.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Nao conectado ao banco");
+        }
+    }            
+
 }
