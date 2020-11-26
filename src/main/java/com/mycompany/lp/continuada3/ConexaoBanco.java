d41973dab3d64759156621d8fe7c8f2a26b1946c
Aleyucra74 <alexandre.yucra@outlook.com>
2020-11-26 20:28:22 -0300

    initial commit

---
+++ src/main/java/com/mycompany/lp/continuada3/ConexaoBanco.java
@@ -0,0 +1,14 @@
+package com.mycompany.lp.continuada3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBanco {
+
+    private final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private final String URL = "";
+    private final String USERNAME = "adminlocal";
+    private final String PASSWORD = "#Gfgrupo11c";
+   
+}
